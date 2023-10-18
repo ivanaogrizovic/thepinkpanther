fc6cd3c7992b3feb5f78a530a914f8ab94c84556
IvanaO <82397055+ivanaogrizovic@users.noreply.github.com>
2023-10-18 14:33:24 +0100

    an undefined chunk of work

--- pink-panther/src/components/shop-by-cut/shop-by-cut.tsx
+++ pink-panther/src/components/shop-by-cut/shop-by-cut.tsx
@@ -1,10 +1,72 @@
 import React from "react";
 import './shop-by-cut.css';
 
+import cutOval from '../../assets/cut-oval.png';
+import cutPrincess from '../../assets/cut-princess.png';
+import cutEmerald from '../../assets/cut-emerald.png';
+import cutPear from '../../assets/cut-pear.png';
+import cutCushion from '../../assets/cut-cushion.png';
+import cutRound from '../../assets/cut-cushion.png';
+import cutMarquise from '../../assets/cut-marquise.png';
+
 function ShopByCut(){
     return(
-        <p>Shop by cut placeholder</p>
-    )
+        <div className='ShopByCut'>
+            <h3>SHOP ENGAGEMENT RINGS BY SHAPE</h3>
+            <div className='shapes-wrapper'>
+               <ul className='shapes-list'>
+                <li className='single-shape'>
+                  <a href=''>
+                  <img className='single-shape__image' src={cutOval}></img>
+                  <p>Oval</p>
+                  </a>
+                </li>
+
+                <li className='single-shape'>
+                  <a href=''>
+                  <img className='single-shape__image'src={cutPrincess}></img>
+                  <p>Princess</p>
+                  </a>
+                </li>
+
+                <li className='single-shape'>
+                  <a href=''>
+                  <img className='single-shape__image'src={cutEmerald}></img>
+                  <p>Emerald</p>
+                  </a>
+                </li>
+
+                <li className='single-shape'>
+                  <a href=''>
+                  <img className='single-shape__image' src={cutPear}></img>
+                  <p>Pear</p>
+                  </a>
+                </li>
+
+                <li className='single-shape'>
+                  <a href=''>
+                  <img className='single-shape__image' src={cutCushion}></img>
+                  <p>Cushion</p>
+                  </a>
+                </li>
+
+                <li className='single-shape'>
+                  <a href=''>
+                  <img className='single-shape__image' src={cutRound}></img>
+                  <p>Round</p>
+                  </a>
+                </li>
+
+                <li className='single-shape'>
+                  <a href=''>
+                  <img className='single-shape__image' src={cutMarquise}></img>
+                  <p>Marquise</p>
+                  </a>
+                </li>
+               </ul>
+            </div>
+        </div>
+    );
 }
 
 export default ShopByCut;
