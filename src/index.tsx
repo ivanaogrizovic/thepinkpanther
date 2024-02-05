aec2f4a8a718cbd9e4df920b4829a033ad93d71c
IvanaO <82397055+ivanaogrizovic@users.noreply.github.com>
2024-02-05 16:08:36 +0000

    finxing path resolution

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from "react-router-dom";
 
 import './index.css';
 import App from './App';
@@ -12,11 +13,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <ProductsProvider>
         <App />
       </ProductsProvider>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
 
