93b9ea6e105e2a4fa9c6c68e62f9a5ab1b4a8793
Ivana <ivanaogrizovic.dev@gmail.com>
2025-11-12 14:45:37 +0000

    refactor: coming soon page

--- src/routes/coming-soon.tsx
+++ src/routes/coming-soon.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import Hero from '../components/hero/hero';
+import weddingBands from '../assets/wedding-bands.webp';
+import fatCat from '../assets/fat-cat.webp';
 
 function ComingSoon() {
 
     return (
-        <div className='pinkpanther-shop -fade-in'>
-            <h3>This section is coming soon, stay tuned</h3>
-        </div>
+        <>
+            <Hero image={weddingBands} />
+            <div className='pinkpanther-coming-soon -fade-in'>
+                <h3>This section is coming soon, stay tuned</h3>
+                <p>In the meantime, enjoy this picture of a cat, and a link to the <Link to='shop' className='pinkpanther-cta-link'>Homepage →</Link></p>
+                <img className='pinkpanther-placeholder -fade-in' src={fatCat} alt=""></img>
+            </div>
+        </>
     );
 }
 
