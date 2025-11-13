cc54292a1670d50c0955c0946ea298f0874636a6
Ivana <ivanaogrizovic.dev@gmail.com>
2025-11-13 16:51:27 +0000

    feat: animate modal

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -4,7 +4,7 @@ function Modal ({closeModal, selectedImage} :any) {
 
     return(
         <div className='pinkpanther-modal-overlay' onClick={closeModal}>
-            <div className='pinkpanther-modal-content' onClick={(e) => e.stopPropagation()}>
+            <div className='pinkpanther-modal-content -zoom-fade-in' onClick={(e) => e.stopPropagation()}>
                 <img src={selectedImage} alt='' className='pinkpanther-modal-image' />
                 <button className='pinkpanther-modal-close' onClick={closeModal}>
                 ✕
