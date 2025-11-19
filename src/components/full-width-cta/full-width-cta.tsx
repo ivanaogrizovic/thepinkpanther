169673bacf1f2fd9814d1acabeaf03755b85555c
Ivana <ivanaogrizovic.dev@gmail.com>
2025-11-19 15:24:12 +0000

    refactor: add prop types to fullWidthCTA

--- src/components/full-width-cta/full-width-cta.tsx
+++ src/components/full-width-cta/full-width-cta.tsx
@@ -1,13 +1,20 @@
 import { Link } from "react-router-dom";
 import './full-width-cta.scss';
 
-function FullWdithCta({ title, text, linkText, link }: any) {
+type FullWidthCtaProps = {
+  title: string;
+  text: string;
+  linkText?: string;
+  link?: string;
+};
+
+function FullWdithCta({ title, text, linkText, link }: FullWidthCtaProps) {
 
     return (
         <div className="pinkpanther-full-width-cta">
             <h2 className="pinkpanther-full-width-cta-title">{title}</h2>
             <p className="pinkpanther-full-width-cta-text">{text}</p>
-            {link && linkText && <p><Link to={link} target="blank" rel="noreferrer" className='pinkpanther-cta-link'>{linkText} →</Link></p>}
+            {link && linkText && <p><Link to={link} target="_blank" rel="noreferrer" className='pinkpanther-cta-link'>{linkText} →</Link></p>}
         </div>
     );
 
