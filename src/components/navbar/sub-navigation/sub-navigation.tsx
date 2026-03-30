50d6bf07284e0ab12964ed9795ef824de5d924ea
IvanaO <82397055+ivanaogrizovic@users.noreply.github.com>
2026-03-30 22:46:17 +0100

    refactor: subnav semantic html

--- src/components/navbar/sub-navigation/sub-navigation.tsx
+++ src/components/navbar/sub-navigation/sub-navigation.tsx
@@ -4,15 +4,25 @@ import "../navbar.scss";
 interface SubNavigationProps {
   filter: string;
   list: string[];
+  onClose?: () => void;
 }
 
-export default function SubNavigation({ filter, list }: SubNavigationProps) {
+export default function SubNavigation({
+  filter,
+  list,
+  onClose,
+}: SubNavigationProps) {
   return (
     <ul className="pinkpanther-sub-navigation-list">
-      <span className="-microcopy -bold">{filter}</span>
+      <li className="-microcopy -bold pinkpanther-sub-navigation-title">
+        {filter}
+      </li>
       {list.map((filterName, index) => (
         <li className="pinkpanther-sub-navigation-list-item" key={index}>
-          <Link to={`shop/engagement?${filter}=${filterName}`}>
+          <Link
+            to={`shop/engagement?${filter}=${filterName}`}
+            onClick={onClose}
+          >
             {filterName}
           </Link>
         </li>
