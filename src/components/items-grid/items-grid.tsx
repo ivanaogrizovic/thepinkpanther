1ed04b44303b9690b2048e7a58188084768d2678
IvanaO <82397055+ivanaogrizovic@users.noreply.github.com>
2026-03-30 22:31:57 +0100

    feat: use memo for items grid

--- src/components/items-grid/items-grid.tsx
+++ src/components/items-grid/items-grid.tsx
@@ -1,32 +1,29 @@
 import { Link } from "react-router-dom";
+import { useMemo } from "react";
 import { engagementRing } from "../../interfaces/engagementRing.interface";
 import "./items-grid.scss";
 
 export default function ItemsGrid({ rings }: { rings: engagementRing[] }) {
-  let frontImage = 0;
-  let result: engagementRing[] = [];
+  const frontImage = 0;
 
-  const filterSelectedItem = (name: string) => {
-    result = rings.filter((ring: any) => {
-      return ring.name === name;
+  const ringsByName = useMemo(() => {
+    const map: Record<string, engagementRing[]> = {};
+    rings.forEach((ring) => {
+      map[ring.name] = rings.filter((r) => r.name === ring.name);
     });
-    return result;
-  };
+    return map;
+  }, [rings]);
 
   return (
     <div className="pinkpanther-product-list -fade-in">
-      {rings?.map((ring: engagementRing, index: number) => (
-        <Link
-          to="item-detail"
-          state={filterSelectedItem(ring.name)}
-          key={index}
-        >
+      {rings?.map((ring, index) => (
+        <Link to="item-detail" state={ringsByName[ring.name]} key={index}>
           <div className="pinkpanther-product -fade-in">
             <img
               className="pinkpanther-product-image"
               src={ring?.images[frontImage]}
               alt={ring.name}
-            ></img>
+            />
             <div className="pinkpanther-product-text">
               <p>{ring.name}</p>
               <p className="-microcopy -bold">{ring.price}€</p>
@@ -34,6 +31,7 @@ export default function ItemsGrid({ rings }: { rings: engagementRing[] }) {
           </div>
         </Link>
       ))}
+
       {rings.length === 0 && (
         <div className="-fade-in">
           <h3>Sorry, we don't have what you're looking for</h3>
