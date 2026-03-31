47afd7187752ace16a37e745ebedba7bb4c6f87a
IvanaO <82397055+ivanaogrizovic@users.noreply.github.com>
2026-03-31 20:43:30 +0100

    chore: update dependencies and reportWebVitals syntax

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,5 +1,6 @@
-declare module '*.jpg';
-declare module '*.jpeg';
-declare module '*.png';
-declare module '*.webp';
-declare module '*.gif';
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.png";
+declare module "*.webp";
+declare module "*.gif";
+declare module "*.scss";
