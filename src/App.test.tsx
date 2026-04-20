9e72c20ef9fe1b33cd881d9b03e3acef4a4dcd72
IvanaO <82397055+ivanaogrizovic@users.noreply.github.com>
2026-04-20 15:20:07 +0100

    fix: wrap app.test.tsx in cart provider

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,12 +3,15 @@ import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import { ProductsProvider } from "./context/products.context";
 import App from "./App";
+import { CartProvider } from "./context/cart.context";
 
 test("renders navigation and home page", () => {
   render(
     <MemoryRouter>
       <ProductsProvider>
-        <App />
+        <CartProvider>
+          <App />
+        </CartProvider>
       </ProductsProvider>
     </MemoryRouter>,
   );
